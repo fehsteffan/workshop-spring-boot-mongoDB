b50b2ae2b6dc16499516879e95e864a66a773c95
Felipe Steffan <felip.developer@hotmail.com>
2022-09-10 18:06:29 -0300

    Consulta simples com query methods

--- src/main/java/com/programweb/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/programweb/workshopmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.programweb.workshopmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,7 @@ import com.programweb.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository <Post, String> {
 	
+	List<Post> findByTitleContaining(String text);	
 	
 
 }
--- src/main/java/com/programweb/workshopmongo/services/PostService.java
+++ src/main/java/com/programweb/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.programweb.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,14 @@ import com.programweb.workshopmongo.services.exception.ObjectNotFundException;
 public class PostService {
 	
 	@Autowired
-	private PostRepository repository;
-	
+	private PostRepository repository;	
 		
 	public Post findById(String id) {
 		Optional<Post> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFundException ("Objeto não encontrado"));
 		}	
-	
-	}
+	public List<Post> findByTitle(String text) {
+		return repository.findByTitleContaining(text);
+	}	
+}
 
